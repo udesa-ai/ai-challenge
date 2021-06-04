36d93288e69eb2413ff787fb543555a2aced0f11
GabrielT <gabriel0torre@gmail.com>
2021-06-04 17:50:40 -0300

    Rename Empty team

--- Assets/Teams/EmptyTeam/PlayerOne.cs
+++ Assets/Teams/EmptyTeam/PlayerOne.cs
@@ -1,10 +1,11 @@
-using Core.Games;
+﻿using Core.Games;
 using Core.Player;
 using Core.Utils;
+using UnityEngine;
 
 namespace Teams.ParticipantTeam
 {
-    public class Offensive : TeamPlayer
+    public class PlayerOne : TeamPlayer
     {
         public override void OnUpdate()
         {
@@ -13,15 +14,16 @@ namespace Teams.ParticipantTeam
 
         public override void OnReachBall()
         {
-
+            
         }
 
         public override void OnScoreBoardChanged(ScoreBoard scoreBoard)
         {
+
         }
 
-        public override FieldPosition GetInitialPosition() => FieldPosition.C2;
+        public override FieldPosition GetInitialPosition() => FieldPosition.A2;
 
-        public override string GetPlayerDisplayName() => "Offensive";
+        public override string GetPlayerDisplayName() => "Player 1";
     }
 }
---
+++ Assets/Teams/EmptyTeam/PlayerThree.cs
@@ -0,0 +1,29 @@
+using Core.Games;
+using Core.Player;
+using Core.Utils;
+using UnityEngine;
+
+namespace Teams.ParticipantTeam
+{
+    public class PlayerThree : TeamPlayer
+    {
+        public override void OnUpdate()
+        {
+
+        }
+
+        public override void OnReachBall()
+        {
+
+        }
+
+        public override void OnScoreBoardChanged(ScoreBoard scoreBoard)
+        {
+
+        }
+
+        public override FieldPosition GetInitialPosition() => FieldPosition.C2;
+
+        public override string GetPlayerDisplayName() => "Player 3";
+    }
+}
