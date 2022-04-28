0207e0ab791f4e7834e8870ca3bc79dc49145a4e
juan.rodriguez.etermax <juan.rodriguez@etermax.com.uy>
2022-04-28 16:35:15 -0300

    scaled image container to be 1:1 with source always

--- Assets/Scripts/TournamentViewLoader.cs
+++ Assets/Scripts/TournamentViewLoader.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class TournamentViewLoader : MonoBehaviour
 {
     [SerializeField] Image viewport;
+    [SerializeField] LayoutElement scalableContainer;
     [SerializeField] string tournament;
     [SerializeField] WebHelper web;
     void Start()
@@ -38,16 +39,15 @@ public class TournamentViewLoader : MonoBehaviour
 
     IEnumerator GetPlayerImage(string url)
     {
-        UnityWebRequest www = UnityWebRequest.Get(url);
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
         yield return www.SendWebRequest();
-        var rawSprite = www.downloadHandler.data;
-
-        Texture2D myTexture = new Texture2D(1920, 1080);
-        myTexture.LoadImage(rawSprite);
+        var myTexture = DownloadHandlerTexture.GetContent(www);
 
         Rect rec = new Rect(0, 0, myTexture.width, myTexture.height);
         Sprite spriteToUse = Sprite.Create(myTexture, rec, new Vector2(0.5f, 0.5f), 100);
 
         viewport.sprite = spriteToUse;
+        scalableContainer.minWidth = myTexture.width;
+        scalableContainer.minHeight = myTexture.height;
     }
 }
